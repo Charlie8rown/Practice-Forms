ba351d1a77b4230891886cea1a8259eabe482db5
Charlie8rown <charlesaharrielson@yahoo.com>
2023-03-01 22:03:51 +0000

    Update ContactUs.js

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -1,33 +1,59 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function ContactUs() {
+function ContactUs(props) {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
+  const [phoneType, setPhoneType] = useState("");
+  const [comments, setComments] = useState("");
+  const [validationErrors, setValidationErrors] = useState([]);
+  const [hasSubmitted, setHasSubmitted] = useState(false);
+
+  useEffect(() => {
+    const errors = [];
+    if (!name.length) errors.push("Please enter your Name");
+    if (!email.includes("@")) errors.push("Please provide a valid Email");
+    setValidationErrors(errors);
+  }, [name, email]);
 
   const onSubmit = (e) => {
-    // Prevent the default form behavior so the page doesn't reload.
     e.preventDefault();
 
-    // Create a new object for the contact us information.
+    setHasSubmitted(true);
+    if (validationErrors.length) return alert(`Cannot Submit`);
+
     const contactUsInformation = {
       name,
       email,
       phone,
+      phoneType,
+      comments,
       submittedOn: new Date(),
     };
 
-    // Ideally, we'd persist this information to a database using a RESTful API.
-    // For now, though, just log the contact us information to the console.
     console.log(contactUsInformation);
     setName("");
     setEmail("");
     setPhone("");
+    setPhoneType("");
+    setComments("");
+    setValidationErrors([]);
+    setHasSubmitted(false);
   };
 
   return (
     <div>
       <h2>Contact Us</h2>
+      {hasSubmitted && validationErrors.length > 0 && (
+        <div>
+          The following errors were found:
+          <ul>
+            {validationErrors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <form onSubmit={onSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
@@ -54,6 +80,27 @@ function ContactUs() {
             type="text"
             onChange={(e) => setPhone(e.target.value)}
             value={phone}
+          />
+          <select
+            name="phoneType"
+            onChange={(e) => setPhoneType(e.target.value)}
+            value={phoneType}
+          >
+            <option value="" disabled>
+              Select a phone type...
+            </option>
+            <option>Home</option>
+            <option>Work</option>
+            <option>Mobile</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="comments">Comments:</label>
+          <textarea
+            id="comments"
+            name="comments"
+            onChange={(e) => setComments(e.target.value)}
+            value={comments}
           />
         </div>
         <button>Submit</button>
